de5eef0d8a05cccfa5a699a466f07c27eb54d774
Jay <jaybhatade7@gmail.com>
2025-02-23 01:12:21 +0530

    final 4.1

--- src/pages/SeriesPage.jsx
+++ src/pages/SeriesPage.jsx
@@ -14,14 +14,14 @@ function SeriesPage() {
       <SM />
 
     <div className="w-full h-fit min-h-screen bg-gradient-to-b from-black via-[#2296ad]/20 to-black text-white">
-    <SeriesScroll series={seriesList} startIndex={0} endIndex={10} headingTitle={"Series Collection"}/>
-    <SeriesScroll series={seriesList} startIndex={10} endIndex={20} headingTitle={"Series Collection"}/>
-    <SeriesScroll series={seriesList} startIndex={20} endIndex={30} headingTitle={"Series Collection"}/>
-    <SeriesScroll series={seriesList} startIndex={30} endIndex={40} headingTitle={"Series Collection"}/>
-    <SeriesScroll series={seriesList} startIndex={40} endIndex={50} headingTitle={"Series Collection"}/>
-    <SeriesScroll series={seriesList} startIndex={50} endIndex={60} headingTitle={"Series Collection"}/>
-    <SeriesScroll series={seriesList} startIndex={60} endIndex={70} headingTitle={"Series Collection"}/>
-    <SeriesScroll series={seriesList} startIndex={70} endIndex={80} headingTitle={"Series Collection"}/>
+    <SeriesScroll series={seriesList} startIndex={0} endIndex={10} headingTitle={"Trending Anime"}/>
+    <SeriesScroll series={seriesList} startIndex={10} endIndex={20} headingTitle={"Action Anime"}/>
+    <SeriesScroll series={seriesList} startIndex={20} endIndex={30} headingTitle={"New Anime Series"}/>
+    <SeriesScroll series={seriesList} startIndex={30} endIndex={40} headingTitle={"Hollywood Series"}/>
+    <SeriesScroll series={seriesList} startIndex={40} endIndex={50} headingTitle={"Currently Trending"}/>
+    <SeriesScroll series={seriesList} startIndex={50} endIndex={60} headingTitle={"Top Rated Series"}/>
+    <SeriesScroll series={seriesList} startIndex={60} endIndex={70} headingTitle={"Bollywood Series"}/>
+    
 
     </div>
     </div>
